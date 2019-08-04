8276481d8c9ac3a89edc7afb076518ae484d576b
Valentino Wong <valentino.wong@gmail.com>
2019-08-03 21:25:30 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Valentino."
+    end
+
+    get '/hometown' do
+        "My hometown is New York City!"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is blah blah blah."
+    end
+
 end
